f37723336e76cc98972d1779b0131ba167bf4946
Phuc Le <phuclh93@gmail.com>
2021-02-05 16:40:08 -0800

    Move Bookings models, factories, migrations to package

---
+++ database/factories/BookingFactory.php
@@ -0,0 +1,30 @@
+<?php namespace Tipoff\Bookings\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Tipoff\Bookings\Models\Booking;
+
+class BookingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Booking::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'order_id'     => randomOrCreate(config('tipoff.model_class.order')),
+            // @todo need to limit slots to just the ones for a room at the location of the order.
+            'slot_id'      => randomOrCreate(config('tipoff.model_class.slot')),
+            'participants' => $this->faker->numberBetween(1, 10),
+            'is_private'   => $this->faker->boolean,
+        ];
+    }
+}
