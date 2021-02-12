9086308777244aaa51b1019b3e9d6881223e4560
Drew Roberts <github@drewroberts.com>
2021-02-12 15:10:07 -0500

    Extend tipoff service provider

--- src/BookingsServiceProvider.php
+++ src/BookingsServiceProvider.php
@@ -2,26 +2,14 @@
 
 namespace Tipoff\Bookings;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Tipoff\Discounts\Models\Booking;
+use Tipoff\Support\TipoffPackage;
+use Tipoff\Support\TipoffServiceProvider;
 
-class BookingsServiceProvider extends PackageServiceProvider
+class BookingsServiceProvider extends TipoffServiceProvider
 {
-    public function boot()
+    public function configureTipoffPackage(TipoffPackage $package): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-
-        parent::boot();
-    }
-
-    public function configurePackage(Package $package): void
-    {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->hasModelInterfaces([
                 BookingInterface::class => Booking::class,
