f570aadce191dfd8539e5d6cae99048cb751998f
Michael Slowik <slowik.michael@gmail.com>
2021-03-02 13:20:53 -0500

    index added to user relationship

--- database/migrations/2020_05_11_100000_create_participants_table.php
+++ database/migrations/2020_05_11_100000_create_participants_table.php
@@ -12,7 +12,7 @@ class CreateParticipantsTable extends Migration
     {
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(app('user'))->nullable();
+            $table->foreignIdFor(app('user'))->nullable()->index();
             $table->string('email')->unique();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
