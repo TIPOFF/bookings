cbb6e1fa5a77d559f959be547ca20c5316a945c8
arwaawan <awan.arwa@aerocareusa.com>
2021-03-02 19:03:03 -0600

    [TGER-79] - use new BasePermissionsMigration

--- database/migrations/2021_02_12_100000_add_booking_permissions.php
+++ database/migrations/2021_02_12_100000_add_booking_permissions.php
@@ -2,24 +2,18 @@
 
 declare(strict_types=1);
 
-use Illuminate\Database\Migrations\Migration;
-use Spatie\Permission\Contracts\Permission;
-use Spatie\Permission\PermissionRegistrar;
+use Tipoff\Authorization\Permissions\BasePermissionsMigration;
 
-class AddBookingPermissions extends Migration
+class AddBookingPermissions extends BasePermissionsMigration
 {
     public function up()
     {
-        if (app()->has(Permission::class)) {
-            app(PermissionRegistrar::class)->forgetCachedPermissions();
+        $permissions = [
+            'view bookings',
+            'create bookings',
+            'update bookings'
+        ];
 
-            foreach ([
-                         'view bookings',
-                         'create bookings',
-                         'update bookings'
-                     ] as $name) {
-                app(Permission::class)::findOrCreate($name, null);
-            };
-        }
+        $this->createPermissions($permissions);
     }
 }
