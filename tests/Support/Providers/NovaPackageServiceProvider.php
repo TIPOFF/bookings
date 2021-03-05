ceeb3ebb2352b038537f73c2ad64cf9f1f3d452c
wolfrednicolas <wolfrednicolas@users.noreply.github.com>
2021-03-05 14:53:48 +0000

    Fix styling

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Tipoff\Bookings\Tests\Support\Providers;
 
+use Tipoff\Bookings\Nova\Booking;
 use Tipoff\Bookings\Nova\Participant;
 use Tipoff\Bookings\Nova\Slot;
-use Tipoff\Bookings\Nova\Booking;
 use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
