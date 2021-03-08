72e79c7115947af0a1f9a500b9d3acebab6e89ec
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-08 18:02:01 -0500

    remove Slot from the BookingServiceProvider

--- src/BookingsServiceProvider.php
+++ src/BookingsServiceProvider.php
@@ -6,7 +6,6 @@ namespace Tipoff\Bookings;
 
 use Tipoff\Bookings\Models\Booking;
 use Tipoff\Bookings\Models\Participant;
-use Tipoff\Bookings\Models\Slot;
 use Tipoff\Bookings\Policies\BookingPolicy;
 use Tipoff\Bookings\Policies\ParticipantPolicy;
 use Tipoff\Support\TipoffPackage;
@@ -20,12 +19,10 @@ class BookingsServiceProvider extends TipoffServiceProvider
             ->hasPolicies([
                 Booking::class => BookingPolicy::class,
                 Participant::class => ParticipantPolicy::class,
-                Slot::class => Slot::class,
             ])
             ->hasNovaResources([
                 \Tipoff\Bookings\Nova\Booking::class,
                 \Tipoff\Bookings\Nova\Participant::class,
-                \Tipoff\Bookings\Nova\Slot::class,
             ])
             ->name('bookings')
             ->hasConfigFile();
