52fd0f68e7c9a700691b36dde0b5700366d10b90
Chris H <crishack8@gmail.com>
2021-03-08 19:25:49 -0600

    Add Rate to NovaServiceProvider #42

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -6,6 +6,7 @@ namespace Tipoff\Bookings\Tests\Support\Providers;
 
 use Tipoff\Bookings\Nova\Booking;
 use Tipoff\Bookings\Nova\Participant;
+use Tipoff\Bookings\Nova\Rate;
 use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
@@ -13,5 +14,6 @@ class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
     public static array $packageResources = [
         Booking::class,
         Participant::class,
+        Rate::class
     ];
 }
