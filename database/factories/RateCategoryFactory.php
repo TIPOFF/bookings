150f8527f29dd8ff7d326ad5f85322082cf62568
Chris H <crishack8@gmail.com>
2021-03-08 18:17:56 -0600

    Rate Category #42

---
+++ database/factories/RateCategoryFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Bookings\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Tipoff\Bookings\Models\RateCategory;
+
+class RateCategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = RateCategory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function definition()
+    {
+        return [
+            'slug' => $this->faker->url,
+            'name' => $this->faker->name
+        ];
+    }
+}
