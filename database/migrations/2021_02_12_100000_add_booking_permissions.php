14d9c3a06f4f2d205beec62d51408940408b4448
arwaawan <awan.arwa@aerocareusa.com>
2021-03-09 13:30:15 -0600

    add user roles to permissions migration

--- database/migrations/2021_02_12_100000_add_booking_permissions.php
+++ database/migrations/2021_02_12_100000_add_booking_permissions.php
@@ -9,13 +9,13 @@ class AddBookingPermissions extends BasePermissionsMigration
     public function up()
     {
         $permissions = [
-            'view bookings',
-            'create bookings',
-            'update bookings',
-            'view participants',
-            'view slots',
-            'create slots',
-            'update slots',
+            'view bookings' => ['Owner', 'Staff'],
+            'create bookings' => ['Owner'],
+            'update bookings' => ['Owner'],
+            'view participants' => ['Owner', 'Staff'],
+            'view slots' => ['Owner', 'Staff'],
+            'create slots' => ['Owner'],
+            'update slots' => ['Owner'],
         ];
 
         $this->createPermissions($permissions);
