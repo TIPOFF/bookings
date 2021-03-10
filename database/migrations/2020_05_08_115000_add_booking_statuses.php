b6d1294b4249696c1ed3c9770257b0cff8e8fb1a
Drew Roberts <github@drewroberts.com>
2021-03-10 12:51:36 -0500

    Publish booking status method #41

--- database/migrations/2020_05_08_115000_add_booking_statuses.php
+++ database/migrations/2020_05_08_115000_add_booking_statuses.php
@@ -9,24 +9,10 @@ class AddBookingStatuses extends Migration
 {
     public function up()
     {
-        foreach([
-                [
-                    'slug'          => 'bookings-pending',
-                    'name'          => 'Pending Booking',
-                    'applies_to'    => 'booking'
-                ],
-                [
-                    'slug'          => 'bookings-cancelled',
-                    'name'          => 'Pending Cancelled',
-                    'applies_to'    => 'booking'
-                ],
-                [
-                    'slug'          => 'bookings-confirmed',
-                    'name'          => 'Pending Confirmed',
-                    'applies_to'    => 'booking'
-                ]
-            ] as $status) {
-            Status::findOrCreate($status);
-        }
+        Status::publishStatuses('booking', [
+            'Pending',
+            'Cancelled',
+            'Confirmed',
+        ])
     }
 }
