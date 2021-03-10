3382c7e12e5e8323893152f1f92c93cf0dfcc2c8
Drew Roberts <github@drewroberts.com>
2021-03-10 10:46:02 -0500

    Rename migration & reorder date right after status

--- database/migrations/2020_05_08_115000_add_booking_statuses.php
+++ database/migrations/2020_05_08_115000_add_booking_statuses.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Tipoff\Statuses\Models\Status;
 
-class CreateBookingStatusesTable extends Migration
+class AddBookingStatuses extends Migration
 {
     public function up()
     {
