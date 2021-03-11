0187defade59d79f3fa765b7cbae4f31eaa01eea
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-11 17:22:31 -0500

    add novaPackageServiceProvider missing

--- tests/Support/Providers/NovaPackageServiceProvider.php
+++ tests/Support/Providers/NovaPackageServiceProvider.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace Tipoff\Bookings\Tests\Support\Providers;
 
 use Tipoff\Bookings\Nova\Booking;
+use Tipoff\Bookings\Nova\BookingCategory;
 use Tipoff\Bookings\Nova\Participant;
 use Tipoff\Bookings\Nova\Rate;
+use Tipoff\Bookings\Nova\RateCategory;
 use Tipoff\TestSupport\Providers\BaseNovaPackageServiceProvider;
 
 class NovaPackageServiceProvider extends BaseNovaPackageServiceProvider
 {
     public static array $packageResources = [
         Booking::class,
+        BookingCategory::class,
         Participant::class,
         Rate::class,
+        RateCategory::class,
     ];
 }
