8da5ef37d6a6df7e1d9ea06f582deeed8685438f
Drew Roberts <github@drewroberts.com>
2021-03-21 20:49:26 -0400

    Update factory for booking

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -11,40 +11,27 @@ use Tipoff\Bookings\Models\Rate;
 
 class BookingFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Booking::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
             'slot_number'           => $this->faker->numberBetween(0, 10),
+            'user_id'               => randomOrCreate(app('user')),
+            'order_id'              => randomOrCreate(app('order')),
             'total_participants'    => $this->faker->numberBetween(0, 10),
             'total_amount'          => $this->faker->numberBetween(0, 10),
             'amount'                => $this->faker->numberBetween(0, 10),
             'total_taxes'           => $this->faker->numberBetween(0, 10),
             'total_fees'            => $this->faker->numberBetween(0, 10),
             'booking_category_id'   => randomOrCreate(app('booking_category')),
-            'rate_id'               => randomOrCreate(app('booking_category')), //TODO change class
+            'rate_id'               => randomOrCreate(app('rate')),
             'experience_id'         => $this->faker->numberBetween(0, 10),
-            'experience_type'       => 'Game',
-            'order_id'              => $this->faker->numberBetween(0, 10),
-            'order_type'            => 'Order',
-            'booking_status_id'     => randomOrCreate(app('status')),
+            'experience_type'       => 'EscaperoomSlot',
             'agent_id'              => $this->faker->numberBetween(0, 10),
             'agent_type'            => 'Agent',
-            'user_id'               => $this->faker->numberBetween(0, 10),
-            'user_type'             => 'User',
             'subject_id'            => $this->faker->numberBetween(0, 10),
-            'subject_type'          => 'Subject',        
+            'subject_type'          => 'Room',        
             'proccessed_at'         => $this->faker->dateTimeBetween('+0 days', '+2 years'),        
             'canceled_at'           => $this->faker->dateTimeBetween('+0 days', '+2 years'),        
         ];
