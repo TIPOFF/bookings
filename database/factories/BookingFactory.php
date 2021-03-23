4b38c877051c698b6c5b8f208cee8d402e4dfc89
Michael Slowik <slowik.michael@gmail.com>
2021-03-23 13:54:36 -0500

    Booking subject/experience factory handling

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -15,6 +15,7 @@ class BookingFactory extends Factory
 
     public function definition()
     {
+
         return [
             'slot_number'           => $this->faker->numberBetween(0, 10),
             'user_id'               => randomOrCreate(app('user')),
@@ -26,14 +27,63 @@ class BookingFactory extends Factory
             'total_fees'            => $this->faker->numberBetween(0, 10),
             'booking_category_id'   => randomOrCreate(app('booking_category')),
             'rate_id'               => randomOrCreate(app('rate')),
-            'experience_id'         => randomOrCreate(app('escaperoom_slot')),
-            'experience_type'       => get_class(app('escaperoom_slot')),
             'agent_id'              => randomOrCreate(app('user')),
             'agent_type'            => get_class(app('user')),
-            'subject_id'            => randomOrCreate(app('room')),
-            'subject_type'          => get_class(app('room')),       
+            'subject_type'          => 'subject',
+            'subject_id'            => 1,
+            'experience_type'       => 'experience',
+            'experience_id'         => 1,
             'proccessed_at'         => $this->faker->dateTimeBetween('+0 days', '+2 years'),        
             'canceled_at'           => $this->faker->dateTimeBetween('+0 days', '+2 years'),        
         ];
     }
+
+    /**
+     * Set booking subject model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function subject($subject)
+    {
+        return $this->state(function (array $attributes) use ($subject) {
+            $subjectModel = app($subject);
+            return [
+                'subject_id'            => $subjectModel->id,
+                'subject_type'          => get_class($subjectModel),
+            ];
+        });
+    }
+
+    /**
+     * Set booking experience model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function experience($experience)
+    {
+        return $this->state(function (array $attributes) use ($experience) {
+            $experienceModel = app($experience);
+            return [
+                'experience_id'            => $experienceModel->id,
+                'experience_type'          => get_class($experienceModel),
+            ];
+        });
+    }
+
+    /**
+     * Use fake experience.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function fakeExperience()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'experience_id'            => 1,
+                'experience_type'          => 'experience',
+            ];
+        });
+    }
+
+
 }
