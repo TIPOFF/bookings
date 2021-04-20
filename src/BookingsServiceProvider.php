b71bdccba37659ebb705a99960caf321ba07dcc0
Patrick Breen <patrick.breen@quartzy.com>
2021-04-20 12:46:26 -0400

    Add missing model interface bindings

--- src/BookingsServiceProvider.php
+++ src/BookingsServiceProvider.php
@@ -6,6 +6,7 @@ namespace Tipoff\Bookings;
 
 use Tipoff\Bookings\Models\Booking;
 use Tipoff\Bookings\Models\BookingCategory;
+use Tipoff\Bookings\Models\BookingSlot;
 use Tipoff\Bookings\Models\Participant;
 use Tipoff\Bookings\Models\Rate;
 use Tipoff\Bookings\Models\RateCategory;
@@ -14,6 +15,12 @@ use Tipoff\Bookings\Policies\BookingPolicy;
 use Tipoff\Bookings\Policies\ParticipantPolicy;
 use Tipoff\Bookings\Policies\RateCategoryPolicy;
 use Tipoff\Bookings\Policies\RatePolicy;
+use Tipoff\Support\Contracts\Booking\BookingCategoryInterface;
+use Tipoff\Support\Contracts\Booking\BookingInterface;
+use Tipoff\Support\Contracts\Booking\BookingParticipantInterface;
+use Tipoff\Support\Contracts\Booking\BookingRateCategoryInterface;
+use Tipoff\Support\Contracts\Booking\BookingRateInterface;
+use Tipoff\Support\Contracts\Booking\BookingSlotInterface;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -33,6 +40,14 @@ class BookingsServiceProvider extends TipoffServiceProvider
                 \Tipoff\Bookings\Nova\Booking::class,
                 \Tipoff\Bookings\Nova\Participant::class,
                 \Tipoff\Bookings\Nova\Rate::class,
+            ])
+            ->hasModelInterfaces([
+                BookingInterface::class => Booking::class,
+                BookingCategoryInterface::class => BookingCategory::class,
+                BookingSlotInterface::class => BookingSlot::class,
+                BookingParticipantInterface::class => Participant::class,
+                BookingRateInterface::class => Rate::class,
+                BookingRateCategoryInterface::class => RateCategory::class,
             ])
             ->hasWebRoute('web')
             ->name('bookings')
