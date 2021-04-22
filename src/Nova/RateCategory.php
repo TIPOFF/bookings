c56b9264f1d7f2e5dd505dce197ca0790ad1ba26
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-04-22 14:07:33 -0400

    add RateCategory Resource

---
+++ src/Nova/RateCategory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tipoff\Bookings\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Slug;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Panel;
+use Tipoff\Support\Nova\BaseResource;
+
+class RateCategory extends BaseResource
+{
+    public static $model = \Tipoff\Bookings\Models\RateCategory::class;
+
+    public static $title = 'name';
+
+    public static $search = [
+        'id',
+    ];
+
+    public static $group = 'Operations Units';
+
+    public function fieldsForIndex(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Slug')->sortable(),
+            Text::make('Name')->sortable(),
+            Date::make('Created', 'created_at')->sortable()->exceptOnForms(),
+        ];
+    }
+
+    public function fields(Request $request)
+    {
+        return [
+            Text::make('Name')->required(),
+            Slug::make('Slug')->from('Name'),
+            new Panel('Data Fields', $this->dataFields()),
+        ];
+    }
+
+
+    protected function dataFields(): array
+    {
+        return array_merge(
+            parent::dataFields(),
+            $this->creatorDataFields(),
+            $this->updaterDataFields(),
+        );
+    }
+}
